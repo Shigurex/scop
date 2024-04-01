5fccbfbb8f7a465003a88e8ad63481fdd9a56d3c
Yamato Hokari <yamatohokari@Yamatononotobukkukonpyuta.local>
2024-04-01 21:45:42 +0900

    fixed bugs for shader

--- resource/src/shader.rs
+++ resource/src/shader.rs
@@ -1,11 +1,11 @@
 use std::{ffi::CString, fs::File, io::Read};
 
 use anyhow::Result;
-use gl::types::GLuint;
+use gl::types::{GLchar, GLint};
 use std::ptr;
 
 pub struct Shader {
-    id: GLuint,
+    id: u32,
 }
 
 impl Shader {
@@ -18,11 +18,6 @@ impl Shader {
         let mut vertex_source = String::new();
         let mut fragment_source = String::new();
 
-        let vertex_source_cstr =
-            CString::new(vertex_source.as_bytes()).map_err(anyhow::Error::msg)?;
-        let fragment_source_cstr =
-            CString::new(fragment_source.as_bytes()).map_err(anyhow::Error::msg)?;
-
         vertex_file
             .read_to_string(&mut vertex_source)
             .map_err(anyhow::Error::msg)?;
@@ -30,20 +25,27 @@ impl Shader {
             .read_to_string(&mut fragment_source)
             .map_err(anyhow::Error::msg)?;
 
+        let vertex_source_cstr =
+            CString::new(vertex_source.as_bytes()).map_err(anyhow::Error::msg)?;
+        let fragment_source_cstr =
+            CString::new(fragment_source.as_bytes()).map_err(anyhow::Error::msg)?;
+
         unsafe {
             let vertex = gl::CreateShader(gl::VERTEX_SHADER);
             gl::ShaderSource(vertex, 1, &vertex_source_cstr.as_ptr(), ptr::null());
             gl::CompileShader(vertex);
-            // shader.check_compile_errors(vertex_shader, "vertex");
+            // shader.check_compile_errors(vertex, "VERTEX");
 
             let fragment = gl::CreateShader(gl::FRAGMENT_SHADER);
             gl::ShaderSource(fragment, 1, &fragment_source_cstr.as_ptr(), ptr::null());
             gl::CompileShader(fragment);
+            // shader.check_compile_errors(fragment, "FRAGMENT");
 
             shader.id = gl::CreateProgram();
             gl::AttachShader(shader.id, vertex);
             gl::AttachShader(shader.id, fragment);
             gl::LinkProgram(shader.id);
+            // shader.check_compile_errors(shader.id, "PROGRAM");
 
             gl::DeleteShader(vertex);
             gl::DeleteShader(fragment);
@@ -55,4 +57,48 @@ impl Shader {
     pub unsafe fn use_shader(&self) {
         gl::UseProgram(self.id)
     }
+
+    // ERROR: NEED TO MODIFY CODE
+    // unsafe fn check_compile_errors(&self, shader: u32, type_: &str) {
+    //     let mut success = gl::FALSE as GLint;
+    //     let mut info_log = Vec::with_capacity(1024);
+    //     info_log.set_len(1024 - 1); // subtract 1 to skip the trailing null character
+    //     if type_ != "PROGRAM" {
+    //         gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut success);
+    //         if success != gl::TRUE as GLint {
+    //             gl::GetShaderInfoLog(
+    //                 shader,
+    //                 1024,
+    //                 ptr::null_mut(),
+    //                 info_log.as_mut_ptr() as *mut GLchar,
+    //             );
+    //             let info_log_string = match String::from_utf8(info_log) {
+    //                 Ok(log) => log,
+    //                 Err(vec) => panic!("failed to convert to compilation log from buffer: {}", vec),
+    //             };
+    //             println!(
+    //                 "failed to compile shader code: type={}, log={}",
+    //                 type_, info_log_string
+    //             );
+    //         }
+    //     } else {
+    //         gl::GetProgramiv(shader, gl::LINK_STATUS, &mut success);
+    //         if success != gl::TRUE as GLint {
+    //             gl::GetProgramInfoLog(
+    //                 shader,
+    //                 1024,
+    //                 ptr::null_mut(),
+    //                 info_log.as_mut_ptr() as *mut GLchar,
+    //             );
+    //             let info_log_string = match String::from_utf8(info_log) {
+    //                 Ok(log) => log,
+    //                 Err(vec) => panic!("failed to convert to link log from buffer: {}", vec),
+    //             };
+    //             println!(
+    //                 "failed to link shader code: type={}, log={}",
+    //                 type_, info_log_string
+    //             );
+    //         }
+    //     }
+    // }
 }
